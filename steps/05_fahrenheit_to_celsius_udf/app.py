cf771e848cc47554f039d6887edf729d626b17f1
Taj Akmal <taj.akmal@snowflake.com>
2023-02-09 11:11:49 -0500

    Regressed the f to celsius function for demo

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -9,41 +9,11 @@
 # SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
-import sys
-from scipy.constants import convert_temperature
-
-def main(temp_f: float) -> float:
-    return convert_temperature(float(temp_f), 'F', 'C')
-    #return (float(temp_f) - 32) * (5/9)
-
-# For local debugging
-# Be aware you may need to type-convert arguments if you add input parameters
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        print(main(*sys.argv[1:]))  # type: ignore
-    else:
-        print(main())  # type: ignore
-
-
-
-# #ORIGINAL CODE
-
-# #------------------------------------------------------------------------------
-# # Hands-On Lab: Data Engineering with Snowpark
-# # Script:       05_fahrenheit_to_celsius_udf/app.py
-# # Author:       Jeremiah Hansen, Caleb Baechtold
-# # Last Updated: 1/9/2023
-# #------------------------------------------------------------------------------
-
-# # SNOWFLAKE ADVANTAGE: Snowpark Python programmability
-# # SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
-# # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
-
 # import sys
+# from scipy.constants import convert_temperature
 
 # def main(temp_f: float) -> float:
-#     return (float(temp_f) - 32) * (5/9)
-
+#     return convert_temperature(float(temp_f), 'F', 'C')
 
 # # For local debugging
 # # Be aware you may need to type-convert arguments if you add input parameters
@@ -52,3 +22,34 @@ if __name__ == '__main__':
 #         print(main(*sys.argv[1:]))  # type: ignore
 #     else:
 #         print(main())  # type: ignore
+
+
+# #------------------------------------------------------------------------------
+
+
+#ORIGINAL CODE
+
+#------------------------------------------------------------------------------
+# Hands-On Lab: Data Engineering with Snowpark
+# Script:       05_fahrenheit_to_celsius_udf/app.py
+# Author:       Jeremiah Hansen, Caleb Baechtold
+# Last Updated: 1/9/2023
+#------------------------------------------------------------------------------
+
+# SNOWFLAKE ADVANTAGE: Snowpark Python programmability
+# SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
+# SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
+
+import sys
+
+def main(temp_f: float) -> float:
+    return (float(temp_f) - 32) * (5/9)
+
+
+# For local debugging
+# Be aware you may need to type-convert arguments if you add input parameters
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(main(*sys.argv[1:]))  # type: ignore
+    else:
+        print(main())  # type: ignore
